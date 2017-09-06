96fdeb689c413870ed4724017db2725f9e84e7e8
David Burles <dburles@gmail.com>
2017-09-07 09:54:25 +1000

    add source

---
+++ index.js
@@ -0,0 +1,31 @@
+export default function state(name, setter, value) {
+  const subscriptions = [];
+
+  function subscribe(fn) {
+    subscriptions.push(fn);
+    return function() {
+      subscriptions.splice(subscriptions.indexOf(fn), 1);
+    };
+  }
+
+  return function(WrappedComponent) {
+    return class extends Component {
+      subscription = subscribe(() => this.setState({}));
+
+      componentWillUnmount() {
+        this.subscription();
+      }
+
+      render() {
+        return React.createElement(WrappedComponent, {
+          ...this.props,
+          [setter]: newValue => {
+            value = newValue;
+            subscriptions.forEach(fn => fn());
+          },
+          [name]: value,
+        });
+      }
+    };
+  };
+}
