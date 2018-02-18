6e0112bb4d9fbffc546d38fd9026ba81419a51f5
David Burles <dburles@gmail.com>
2018-02-18 18:12:34 +1100

    v2

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 export default function State(initialState, setterHandler = value => value) {
   const subscriptions = [];
-  let state = initialState;
   let shouldUpdate = true;
 
   function subscribe(fn) {
@@ -12,16 +11,12 @@ export default function State(initialState, setterHandler = value => value) {
     };
   }
 
-  function get() {
-    return state;
-  }
-
   async function set(setValue, cb) {
     const newValue =
-      typeof setValue === 'function' ? setValue(state) : setValue;
+      typeof setValue === 'function' ? setValue(wrapped.state) : setValue;
 
-    if (newValue !== state) {
-      state = await setterHandler(newValue);
+    if (newValue !== wrapped.state) {
+      wrapped.state = await setterHandler(newValue);
       shouldUpdate = true;
       subscriptions.forEach(fn => fn(cb));
     } else {
@@ -29,10 +24,7 @@ export default function State(initialState, setterHandler = value => value) {
     }
   }
 
-  const container = map => WrappedComponent => {
-    if (typeof map !== 'function') {
-      throw Error('State: map function is required');
-    }
+  const wrapped = map => WrappedComponent => {
     return class extends Component {
       shouldComponentUpdate(nextProps) {
         return nextProps !== this.props || shouldUpdate;
@@ -46,20 +38,24 @@ export default function State(initialState, setterHandler = value => value) {
 
       render() {
         const props = {
-          ...this.props,
-          ...map({
-            state,
-            set,
-          }),
+          ...Object.keys(wrapped).reduce((obj, key) => {
+            obj[key] = wrapped[key];
+            return obj;
+          }, {}),
+          state: wrapped.state,
+          set,
         };
 
-        return React.createElement(WrappedComponent, props);
+        return React.createElement(WrappedComponent, {
+          ...this.props,
+          ...(typeof map === 'function' ? map(props) : props),
+        });
       }
     };
   };
 
-  container.get = get;
-  container.set = set;
+  wrapped.set = set;
+  wrapped.state = initialState;
 
-  return container;
+  return wrapped;
 }
